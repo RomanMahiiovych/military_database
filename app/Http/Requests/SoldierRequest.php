3ee42fd5f13be1b3240c9da95ce926c4063fa8f5
Roman <romanmah1998@gmail.com>
2021-07-27 15:46:21 +0300

    Added a validation for the uploaded photo.

--- app/Http/Requests/SoldierRequest.php
+++ app/Http/Requests/SoldierRequest.php
@@ -30,6 +30,13 @@ class SoldierRequest extends FormRequest
             'email' => 'required|string|email|max:255|unique:soldiers',
             'phone_number' => ['required', 'regex:/^([0-9\s\-\+\(\)]*)$/', 'max:19'],
             'salary' => 'required|min:0|max:500000',
+            'photo' => ['required', 'mimes:jpeg,png', 'max:2048', 'dimensions:min_width=300,min_height=300,max_width=1000,max_height=1000']
+        ];
+    }
+
+    public function messages() {
+        return [
+            'photo.*' => 'File format jpg, png up to 2 MB, the minimum size of 300x300px'
         ];
     }
 }
